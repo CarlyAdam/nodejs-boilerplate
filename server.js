42363afcf418b079113d8148b4550b1b85f2f77c
Cesar Bretana Gonzalez <bretanac@gmail.com>
2019-06-02 16:07:28 +0200

    Removed default connection from testing env

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const db = require('./src/db');
 const config = require('./src/config');
 const debug = require('./src/helpers/debugger');
 
+if (config.appEnv !== 'test') {
+  db.connect(config.dbUrl)
+    .then(() => debug('db', 'Successfully connected to database'))
+    .catch(err => debug('db', err.message));
+}
+
 app.listen(config.appPort, async () => {
   debug('entrypoint', `Server running on port: ${config.appPort}`);
-
-  try {
-    await db.connect(config.dbUrl);
-  } catch (error) {
-    debug(error.message);
-  }
 });
--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-const debug = require('../helpers/debugger');
 
 module.exports.connect = async (dbUrl) => {
   if (!dbUrl) {
     throw new Error('Database url not defined');
   }
   await mongoose.connect(dbUrl, { useNewUrlParser: true });
-  debug('db', 'Successfully connected to database');
 };
